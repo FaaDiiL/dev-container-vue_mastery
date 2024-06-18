1950c399bbdfe69d6684c5f4a19cd307051cd1bc
fadil <your-username@users.noreply.github.com>
2024-06-18 09:01:20 +0200

    Add conditional rednering to elements

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            inventory: 11,
+            onSale: true,
+            inStock: function (){
+                return this.inventory > 10
+            },
+            almostSoldOut: function (){
+                return this.inventory <= 10 && this.inventory > 0
+            },
         }
     }
 })
